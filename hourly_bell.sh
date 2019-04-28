02dc38cf1d3728a417d5501404d743c90c343263
s624 <49605175+s624@users.noreply.github.com>
2019-04-28 22:49:46 +0530

    Update hourly_bell.sh

--- hourly_bell.sh
+++ hourly_bell.sh
@@ -1,33 +1,59 @@
 #!/bin/bash
-
 #
 # to contact use the reddit user -_-raj-_-
-#
 
 # if you dont have sox installed 
 # type sudo apt-get install sox libsox-fmt-all
 
 
-if [ "$1" == "-h" ]; then
-	echo " 1st argument should be the path to the file which would be played \n 2nd argument should be the volume factor"
-	exit
-fi	
+### the colon signifies that the value of that flag is needed.
+### without a colon values on that flag is not needed. What does it mean? IDK. 
+### But the program doesn't work right way without the colons. 
+while getopts 'hf:v:s:m:' opt; do
+    case "${opt}" in 
+    h ) echo " the flags r hf:v:s:m: stands for file Volume seconds and \
+      minnutes, the default values of sec is 60 and min is 1 and music \
+      is bracked " ; exit ;;
+    f ) FILEE=${OPTARG}  ;;
+    v ) Volume=${OPTARG} ;;
+    s ) Sec=${OPTARG}    ;;
+    m ) Min=${OPTARG} ;;
+    esac
+  done 
+### One extra space while puting a value in a variable and the variable is
+### being treated like a command. 
 
+
+### These are the default values of the flags 
+FILEE="${FILEE:=~/Music/short-bells/braked_good.mp3}" 
+Volume="${Volume:=1}" 
+Sec="${Sec:=60}"
+Min="${Min:=1}"
+
+
+let " spend=0 "
 while true; do
-	if [ $(date +%S) -eq 0 ] ; then 
-		let min=$(date +%M)
-		if [ "$(date +%M)" -ne 59 ] && [ "$(date +%M)" -ne 10  ] && [ "$(date +%M)" -ne 20 ] && [ "$(date +%M)" -ne 30 ] && [ "$(date +%M)" -ne 40 ] && [ "$(date +%M)" -ne 49 ] && [ "$(date +%M)" -ne 00 ]  ;  then 
-			if [ $# -gt 1 ] ; then
-				play --volume $2  $1 
-			else
-				play $1
-			fi	
-			sleep 1s #comment without this line the music will go on being played again and again untill the second is over
-		fi	
-	#else
-	#	echo 'not round'	
-	fi
+  let m=$((10#$(date +%M))) ; 
+  let ss=$((10#$(date +%S))); 
+### These 10# before the value signifies that the 
+### value is decimal without this syntax. The value 
+### is being treated as octa and for numbers above 7 
+### it gives an error. 
+  if [ $((${ss}%${Sec})) -eq 0 ] && [ $((${m}%${Min})) -eq 0 ]  ; then 
+		let "spend=spend+1"
+		if [ "$m" -ne 59 ] && [ "$m" -ne 10  ] && [ "$m" -ne 20 ] && \
+      [ "$m" -ne 30 ] && [ "$m" -ne 40 ] && [ "$m" -ne 49 ] ; then 
+			nohup play --volume $Volume  $FILEE > /dev/null 2>/dev/null
+## The > /dev/null and 2> directs the output error to null.  
+		fi 
+			echo $spend	
+			sleep 1s 
+### comment without this sleep line the music will go on being played 
+### again and again untill the second is over
+	fi	
+	sleep 1s
 done
+
 
 
 
